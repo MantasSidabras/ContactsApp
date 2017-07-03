f2951becebbc42a854a62bfa6564dcd8f0431821
mantas.sidabras <mantas.sidabras@LT-LIT-SC-0326>
2017-07-03 17:28:16 +0300

    Minor changes

---
+++ ContactsApp/Contact_Access/ContactAccess.cs
@@ -0,0 +1,24 @@
+﻿using ContactsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsApp.Contact_Access
+{
+    public class ContactAccess
+    {
+        public IEnumerable<Contact> contacts { get; set; }
+
+        public ContactAccess()
+        {
+            contacts = new List<Contact>()
+            {
+                new Contact(){FirstName = "Jonas", LastName = "Motiejauskas", Email = "jonas.motiejauskas@gmail.com", Phone =  "86789456"},
+                new Contact(){FirstName = "Benas" },
+                new Contact(){FirstName = "Povilas"}
+            };
+        }
+
+    }
+}
